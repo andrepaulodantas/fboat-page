e6bd41efd99d33715d970a8ef66f79b9fa40fddb
Andre Paulo Dantas de Araujo <andrepaulodantas@gmail.com>
2022-07-21 16:24:22 -0300

    bug card

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,20 @@
 import { Component } from "react";
+import FormRegistration from "../../components/FormRegistration/FormRegistration";
 
 class Login extends Component {    
+
+    criarNota(titulo, texto){
+        console.log(`uma nova nota foi criada ` + titulo + " " + texto)
+    }
+
     render() { 
-        return ( 
-            <div>
+        return (             
+            <div>                
                 <h1>Login</h1>
                 <p>This is the login page</p>
+                <FormRegistration criarNota={this.criarNota}/>
             </div>
+
         );
     }
 }
